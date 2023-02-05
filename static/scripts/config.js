9beb09abc1392655185e74c620d05f657264d7c4
ANGJustinl <ANGJustinl@users.noreply.github.com>
2023-02-05 16:05:40 +0000

    chore: update config file

--- static/scripts/config.js
+++ static/scripts/config.js
@@ -1,9 +1,27 @@
 export default {
   items: [
+    "meme/JK.png",
+    "meme/JKJK.jpg",
+    "meme/cats.jpg",
+    "meme/sexbot.png",
+    "meme/test",
+    "meme/一般路过dbz.png",
+    "meme/咖喱窝游玩一小时价格.jpg",
+    "meme/咖喱窝真正持有者.jpg",
+    "meme/地暖用法.png",
     "meme/安全带的正确系法.jpg",
     "meme/对实况主导.jpg",
-    "meme/咖喱窝游玩一小时价格.jpg",
-    "meme/过来坐.gif",
+    "meme/我也要玩!.jpg",
     "meme/我喜欢吃屎.jpg",
+    "meme/最速答错传送.jpg",
+    "meme/有你有我也有他.jpg",
+    "meme/王座享有者.jpg",
+    "meme/白鼠学习我apex,下一秒他也来了.jpg",
+    "meme/真正有意义的.jpg",
+    "meme/瞪你.jpg",
+    "meme/超级助理.png",
+    "meme/超级棍.png",
+    "meme/过来坐.gif",
+    "meme/这位群友不要在地上睡觉.jpg"
   ]
 }
